5df5382dc0d9ca9e1ddc75ad891158c40b89430c
Rodrigo Biglio <rodrigogbiglio@gmail.com>
2024-08-18 14:32:02 -0300

    input con h1 bienvenida

--- main.js
+++ main.js
@@ -2,6 +2,22 @@ const saludo = () =>{
     alert("hola");
 }
 
-setTimeout(() => {
-    saludo();
-}, 3000);
+// setTimeout(() => {
+//     saludo();
+// }, 3000);
+
+
+const  titular =document.getElementById("titular");
+const formulario = document.getElementById("formulario");
+
+formulario.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    const inputs = e.target.children;
+    console.log(inputs[0].children[0].value);
+    if (inputs[0].children[0].value === ""){
+        titular.innerHTML="Te dije que pongas tu nombre cabeza de pito"
+    }else{
+        titular.innerHTML='Bienvenido ' + inputs[0].children[0].value;
+    }
+    titular.classList.add("visible");
+})
